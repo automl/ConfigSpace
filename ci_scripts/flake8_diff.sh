606fbf5894703fb4e22b91d41f52e002799f25cb
Thomas Fan <thomasjpfan@gmail.com>
2019-03-11 16:41:28 -0400

    CLN Removes sklearn message in flake8_diff

--- ci_scripts/flake8_diff.sh
+++ ci_scripts/flake8_diff.sh
@@ -115,11 +115,6 @@ echo -e '\nRunning flake8 on the diff in the range' "$COMMIT_RANGE" \
      "($(git rev-list $COMMIT_RANGE | wc -l) commit(s)):"
 echo '--------------------------------------------------------------------------------'
 
-# We ignore files from sklearn/externals. Unfortunately there is no
-# way to do it with flake8 directly (the --exclude does not seem to
-# work with --diff). We could use the exclude magic in the git pathspec
-# ':!sklearn/externals' but it is only available on git 1.9 and Travis
-# uses git 1.8.
 # We need the following command to exit with 0 hence the echo in case
 # there is no match
 MODIFIED_FILES="$(git diff --name-only $COMMIT_RANGE ||  echo "no_match")"
@@ -136,7 +131,7 @@ check_files() {
 }
 
 if [[ "$MODIFIED_FILES" == "no_match" ]]; then
-    echo "No file outside sklearn/externals and doc/sphinxext/sphinx_gallery has been modified"
+    echo "No file has been modified"
 else
 
     check_files "$(echo "$MODIFIED_FILES" | grep -v ^examples)"
