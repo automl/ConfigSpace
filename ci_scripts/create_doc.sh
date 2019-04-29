2165e9ae0b6e081dd5ee2e72fc02b80051a55ba7
PhMueller <muller-phil@gmx.net>
2019-04-29 20:12:43 +0200

    revert documentation target

--- ci_scripts/create_doc.sh
+++ ci_scripts/create_doc.sh
@@ -33,7 +33,7 @@ if ! [[ -z ${DOCPUSH+x} ]]; then
         mkdir $1
 
         # get previous documentation from github
-        git clone https://github.com/PhMueller/ConfigSpace.git --branch gh-pages --single-branch
+        git clone https://github.com/automl/ConfigSpace.git --branch gh-pages --single-branch
 
         # copy previous documentation
         cp -r ConfigSpace/. $1
