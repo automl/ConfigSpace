5027b4a47333f7626e96ef2297c7dbf18825ff3f
Matthias Feurer <feurerm@informatik.uni-freiburg.de>
2021-01-04 10:43:10 +0100

    drop BSD headers where they were left

--- ConfigSpace/read_and_write/pcs_new.py
+++ ConfigSpace/read_and_write/pcs_new.py
@@ -1,24 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-##
-# wrapping: A program making it easy to use hyperparameter
-# optimization software.
-# Copyright (C) 2013 Katharina Eggensperger and Matthias Feurer
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 __authors__ = ["Katharina Eggensperger", "Matthias Feurer", "Christina Hernández Wunsch"]
 __contact__ = "automl.org"
 
