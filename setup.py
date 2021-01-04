5c180915fb5b8a191779f0d81cdb4d613ad04d25
Eddie Bergman <eddiebergmanhs@gmail.com>
2021-01-04 09:41:00 +0000

    Fix compatibility for Python 3.9 gcd import (#170)
    * Fix compatibility for Python 3.9
    
    * Fixed backwards compatibility for gcd import

--- setup.py
+++ setup.py
@@ -54,6 +54,7 @@ MIN_PYTHON_VERSION = '>=3.6'
 CLASSIFIERS = ['Programming Language :: Python :: 3.6',
                'Programming Language :: Python :: 3.7',
                'Programming Language :: Python :: 3.8',
+               'Programming Language :: Python :: 3.9',
                'Development Status :: 4 - Beta',
                'Natural Language :: English',
                'Intended Audience :: Developers',
