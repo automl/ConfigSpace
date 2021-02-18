0374d70f07cd668a9a0859e307eb26cc579ea401
Matthias Feurer <lists@matthiasfeurer.de>
2021-02-18 06:12:44 +0100

    Update test/test_hyperparameters.py

--- test/test_hyperparameters.py
+++ test/test_hyperparameters.py
@@ -1091,7 +1091,7 @@ class TestHyperparameters(unittest.TestCase):
         # test that returned types are correct
         # if size=None, return a value, but if size=1, return a 1-element array
         self.assertIsInstance(f1.rvs(), float)
-        self.assertEqual(type(f1.rvs(size=1)), np.ndarray)
+        self.assertIsInstance(1.rvs(size=1), np.ndarray)
         self.assertEqual(type(f1.rvs(size=2)), np.ndarray)
 
         self.assertAlmostEqual(f1.rvs(random_state=100), f1.rvs(random_state=100))
