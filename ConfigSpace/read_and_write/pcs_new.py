92d89cebbf4c8646fbe1bf770947f7963a1fa9fe
dengdifan <difandeng@gmail.com>
2021-12-08 12:51:14 +0100

    add check for pcs_new read

--- ConfigSpace/read_and_write/pcs_new.py
+++ ConfigSpace/read_and_write/pcs_new.py
@@ -414,11 +414,33 @@ def read(pcs_string, debug=False):
             else:
                 # So far, only equals is supported by SMAC
                 if tmp_list[1] == '=':
-                    # TODO maybe add a check if the hyperparameter is
-                    # actually in the configuration space
+                    hp = configuration_space.get_hyperparameter(tmp_list[0])
+                    if isinstance(hp, NumericalHyperparameter):
+                        if isinstance(hp, IntegerHyperparameter):
+                            forbidden_value = int(tmp_list[2])
+                        elif isinstance(hp, FloatHyperparameter):
+                            forbidden_value = float(tmp_list[2])
+                        if forbidden_value < hp.lower or forbidden_value > hp.upper:
+                            # TODO alternatively we could raise an warning and ignore this forbidden
+                            raise ValueError(f'forbidden_value is set out of the bound, it needs to be set between'
+                                             f'[{hp.lower}, {hp.upper}] but its value is {forbidden_value}')
+                    elif isinstance(hp, (CategoricalHyperparameter, OrdinalHyperparameter)):
+                        hp_values = hp.choices if isinstance(hp, CategoricalHyperparameter) else hp.sequence
+                        forbidden_value_in_hp_values = False
+                        for hp_value in hp_values:
+                            if str(hp_value) == tmp_list[2]:
+                                forbidden_value = hp_value
+                                forbidden_value_in_hp_values = True
+                                break
+                        if not forbidden_value_in_hp_values:
+                            raise ValueError(f'forbidden_value is set out of the bound, it needs to be set as'
+                                             f'one member of {hp_values}  but its value is {forbidden_value}')
+                    else:
+                        raise ValueError('Unsupported Hyperparamter sorts')
+
                     clause_list.append(ForbiddenEqualsClause(
                         configuration_space.get_hyperparameter(tmp_list[0]),
-                        tmp_list[2]))
+                        forbidden_value))
                 else:
                     raise NotImplementedError()
                 tmp_list = []
