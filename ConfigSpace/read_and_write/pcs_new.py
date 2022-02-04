9305fe2402e3323085978f42cab5b2ee6efae5ab
Matthias Feurer <feurerm@informatik.uni-freiburg.de>
2022-02-04 16:32:12 +0100

    PEP8

--- ConfigSpace/read_and_write/pcs_new.py
+++ ConfigSpace/read_and_write/pcs_new.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-PCS (parameter configuration space) is a simple, human-readable file format for the description of an
-algorithm's configurable parameters, their possible values, as well as any
-parameter dependencies. There exist an old and a new version.
+PCS (parameter configuration space) is a simple, human-readable file format for the
+description of an algorithm's configurable parameters, their possible values, as well
+as any parameter dependencies. There exist an old and a new version.
 
 The new PCS format is part of the
 `Algorithm Configuration Library 2.0 <https://bitbucket.org/mlindauer/aclib2/src/master/>`_.
@@ -16,8 +16,8 @@ and further examples are provided in the
 .. note::
 
     The PCS format definition has changed in the year 2016 and is supported by
-    AClib 2.0, as well as SMAC (v2 and v3). To write or to read the **old** version of pcs, please
-    use the :class:`~ConfigSpace.read_and_write.pcs` module.
+    AClib 2.0, as well as SMAC (v2 and v3). To write or to read the **old** version of pcs,
+    please use the :class:`~ConfigSpace.read_and_write.pcs` module.
 """
 
 __authors__ = ["Katharina Eggensperger", "Matthias Feurer", "Christina Hernández Wunsch"]
