175f79883fc02fdb390f0e5a64a9b469c56bc234
Carl Hvarfner <58733990+hvarfner@users.noreply.github.com>
2022-03-21 13:30:05 +0100

    Squashed all the changes related to densities. _pdf works with the (#241)
    shrunk unit hypercube range for the Beta hyperparameter case, and tests
    have been rewritten to accomodate.

--- ConfigSpace/forbidden.pyx
+++ ConfigSpace/forbidden.pyx
@@ -12,7 +12,7 @@
 #       notice, this list of conditions and the following disclaimer in the
 #       documentation and/or other materials provided with the distribution.
 #     * Neither the name of the <organization> nor the
-#       names of its contributors may be used to endorse or promote products
+#       names of itConfigurationSpaces contributors may be used to endorse or promote products
 #       derived from this software without specific prior written permission.
 #
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
