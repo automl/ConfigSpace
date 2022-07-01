46c818c3f1d360d07630b2d11bb8416c449b55c0
eddiebergman <eddiebergmanhs@gmail.com>
2022-07-01 11:51:53 +0200

    Add doc for distributions

--- ConfigSpace/api/distributions.py
+++ ConfigSpace/api/distributions.py
@@ -22,23 +22,46 @@ from dataclasses import dataclass
 @dataclass
 class Distribution:
     """Base distribution type"""
+
     pass
 
 
 @dataclass
 class Uniform(Distribution):
-    """Represents a uniform distribution"""
+    """A uniform distribution"""
+
     pass
 
 
 @dataclass
 class Normal(Distribution):
-    """Represents a normal distribution"""
+    """Represents a normal distribution.
+
+    Parameters
+    ----------
+    mu: float
+        The mean of the distribution
+
+    sigma: float
+        The standard deviation of the float
+    """
+
     mu: float
     sigma: float
 
 
 @dataclass
 class Beta(Distribution):
+    """Represents a beta distribution.
+
+    Parameters
+    ----------
+    alpha: float
+        The alpha parameter of a beta distribution
+
+    beta: float
+        The beta parameter of a beta distribution
+    """
+
     alpha: float
     beta: float
