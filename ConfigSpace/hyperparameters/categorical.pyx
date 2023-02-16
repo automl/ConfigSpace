ae473ccf605fcd80cf4543cf655fe90da7542fa8
herilalaina <rkt.herilalaina@gmail.com>
2023-02-16 19:37:38 +0100

    (categorical hp) change default to the most probable

--- ConfigSpace/hyperparameters/categorical.pyx
+++ ConfigSpace/hyperparameters/categorical.pyx
@@ -236,7 +236,7 @@ cdef class CategoricalHyperparameter(Hyperparameter):
     def check_default(self, default_value: Union[None, str, float, int]
                       ) -> Union[str, float, int]:
         if default_value is None:
-            return self.choices[0]
+            return self.choices[np.argmax(self.weights) if hasattr(self, "weights") else 0]
         elif self.is_legal(default_value):
             return default_value
         else:
