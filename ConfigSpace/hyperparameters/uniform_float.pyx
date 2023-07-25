a817c9bb7c91eef472d9fa35af2e1e3091c96c22
Bruno <bruno.belucci-teixeira@dauphine.eu>
2023-07-25 18:52:12 +0200

    Ensure that type of `default_value` in `UniformFloatHyperparameter` is of type `float` after rounding it. Also ensure that we pass a `float` `default_value` when creating `self.ufhp` inside `UniformIntegerHyperparameter`

--- ConfigSpace/hyperparameters/uniform_float.pyx
+++ ConfigSpace/hyperparameters/uniform_float.pyx
@@ -121,7 +121,7 @@ cdef class UniformFloatHyperparameter(FloatHyperparameter):
                 default_value = np.exp((np.log(self.lower) + np.log(self.upper)) / 2.)
             else:
                 default_value = (self.lower + self.upper) / 2.
-        default_value = np.round(float(default_value), 10)
+        default_value = float(np.round(float(default_value), 10))
 
         if self.is_legal(default_value):
             return default_value
